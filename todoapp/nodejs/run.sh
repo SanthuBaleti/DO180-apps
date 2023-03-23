5e5112409858fc0c61fc296db157d6ed97eb1a8a
SanthoshBaleti <125953052+SanthuBaleti@users.noreply.github.com>
2023-03-23 16:51:57 +0530

    Update run.sh

--- todoapp/nodejs/run.sh
+++ todoapp/nodejs/run.sh
@@ -3,4 +3,13 @@
 source /opt/rh/nodejs010/enable
 
 node app.js
+podman run -d --name mysql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 \
+-e MYSQL_PASSWORD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 \
+-v $PWD/work/data:/var/lib/mysql/data \
+-p 30306:3306 \
+registry.redhat.io/rhel8/mysql-80:1
 
+podman run -d --name todoapi -e MYSQL_DATABASE=items -e MYSQL_USER=user1 \
+-e MYSQL_PASSWORD=mypa55 \
+-p 30080:30080 \
+do180/todonodejs
