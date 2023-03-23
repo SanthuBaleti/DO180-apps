272179eab096fbd4eb2dff2675c898b640cbac15
SanthoshBaleti <125953052+SanthuBaleti@users.noreply.github.com>
2023-03-23 17:29:44 +0530

    update run.sh

--- todoapp/nodejs/run.sh
+++ todoapp/nodejs/run.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+source /opt/rh/nodejs010/enable
+
 podman run -d --name mysql -e MYSQL_DATABASE=items -e MYSQL_USER=user1 \
 -e MYSQL_PASSWORD=mypa55 -e MYSQL_ROOT_PASSWORD=r00tpa55 \
 -v $PWD/work/data:/var/lib/mysql/data \
@@ -8,3 +12,5 @@ podman run -d --name todoapi -e MYSQL_DATABASE=items -e MYSQL_USER=user1 \
 -e MYSQL_PASSWORD=mypa55 \
 -p 30080:30080 \
 do180/todonodejs
+
+node app.js
